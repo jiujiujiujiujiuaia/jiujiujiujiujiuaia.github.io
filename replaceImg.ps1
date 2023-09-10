ad10d6bbbfb524b6db7627430bd5542ceef2c571
jiujiuaia <jiujiuaia@gmail.com>
2023-09-10 19:56:23 +0800

    add log

---
+++ replaceImg.ps1
@@ -0,0 +1,31 @@
+param(
+    [string]$filename,    # 给定文件的文件名
+    [string]$oldText = "(img",        # 目标替换的字符
+    [string]$newText      # 替换的字符
+)
+
+# 基础路径
+$baseFilePath = ".\_posts\"
+$currentDirectory = $pwd.Path
+$fullFilePath = Join-Path $currentDirectory $baseFilePath
+# 构造完整文件路径
+$fullFilePath = Join-Path $fullFilePath $filename
+
+# 确保文件存在
+if (-not (Test-Path $fullFilePath)) {
+    Write-Error "文件 $fullFilePath 不存在!"
+    exit
+}
+
+# 替换 {placeholder} 变量
+$templateUrl = "(https://raw.githubusercontent.com/jiujiujiujiujiuaia/jiujiujiujiujiuaia.github.io/master/_posts/pic/{placeholder}/img"
+$content = $templateUrl.Replace("{placeholder}", $newText)
+
+# 读取文件内容并替换文本
+$fileContent = Get-Content $fullFilePath -Raw -Encoding utf8
+$fileContent = $fileContent.Replace($oldText, $content)
+
+# 写入修改后的内容回文件
+$fileContent | Set-Content $fullFilePath -Encoding utf8
+
+Write-Output "文件 $fullFilePath 中的文本已替换!"
