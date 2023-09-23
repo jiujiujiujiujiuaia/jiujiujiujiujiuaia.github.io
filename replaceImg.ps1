807de2a5c171c547842d8beed5e0dcf789d08f32
jiujiuaia <jiujiuaia@gmail.com>
2023-09-23 11:43:32 +0800

    update script

--- replaceImg.ps1
+++ replaceImg.ps1
@@ -1,7 +1,7 @@
 param(
     [Alias('f')] [string]$filename,
     [string]$oldText = "(img",
-    [Alias('image')] [string]$imageFolder,
+    [Alias('i')] [string]$imageFolder,
     [Alias('c')] [string]$comments = ""
 )
 
@@ -14,7 +14,7 @@ function Replace-Content {
 
     # 确保文件存在
     if (-not (Test-Path $filePath)) {
-        Write-Error "File $filePath doesn't exist!"
+        Write-Error "[Error]File $filePath doesn't exist!"
         return
     }
 
@@ -28,8 +28,47 @@ function Replace-Content {
     # 写入修改后的内容回文件
     $fileContent | Set-Content $filePath -Encoding utf8
 
-    Write-Output "File $filePath has been replaced!"
+    Write-Output "[Completed] File $filePath has been replaced!"
 }
+
+function Move-ImagesToFolder {
+    param(
+        [string]$imageFolder
+    )
+
+    # 确定基础路径
+    $baseDir = "_posts"
+    $currentDirectory = $pwd.Path
+    $baseDir = Join-Path $currentDirectory $baseDir
+
+    $picDir = Join-Path $baseDir "pic"
+    $targetDir = Join-Path $picDir $imageFolder
+
+    # 检查目标文件夹是否存在，如果不存在则创建
+    if (-not (Test-Path $targetDir)) {
+        New-Item -ItemType Directory -Path $targetDir
+    }
+
+    # 查找所有 png 图片
+    $pngFiles = Get-ChildItem -Path $baseDir -Filter *.png
+
+    foreach ($file in $pngFiles) {
+        # 构建目标文件路径
+        $targetFile = Join-Path $targetDir $file.Name
+
+        # 检查目标文件夹中是否存在同名文件
+        if (Test-Path $targetFile) {
+            Write-Output "[Warning] =====$targetFile is existing, please move it manually===="
+            continue
+        }
+
+        # 移动图片到目标文件夹
+        Move-Item -Path $file.FullName -Destination $targetFile
+    }
+
+    Write-Output "[Completed] moving pictures to pic\$imageFolder"
+}
+
 
 function Submit-Commit {
     param(
@@ -39,7 +78,7 @@ function Submit-Commit {
     git add .
     git commit -m $comments
     git push
-    Write-Output "Pushed with comments: $comments"
+    Write-Output "[Completed] Pushed with comments: $comments"
 }
 
 # 基础路径
@@ -53,7 +92,8 @@ $templateUrl = "(https://raw.githubusercontent.com/jiujiujiujiujiuaia/jiujiujiuj
 $newContent = $templateUrl.Replace("{placeholder}", $imageFolder)
 
 # 如果filename不为空，执行字符替换函数
-if ($filename -ne "") {
+if ($filename -ne "" -and $imageFolder -ne "") {
+    Move-ImagesToFolder -imageFolder $imageFolder
     Replace-Content -filePath $fullFilePath -oldText $oldText -newText $newContent
 }
 
